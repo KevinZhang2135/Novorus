a1499e870ef4d1150f23497d874ca2f162af5c3e
Kevin <kevinbczhang@gmail.com>
2022-07-21 00:08:09 -0400

    CSV Files

---
+++ levels/tilesets/knight.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="knight_idle1" tilewidth="24" tileheight="24" tilecount="1" columns="1">
+ <image source="../../sprites/player/idle/knight_idle1.png" width="24" height="24"/>
+</tileset>
