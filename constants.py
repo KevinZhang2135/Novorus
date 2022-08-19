b6cd7e72116cd4bd3d3a0eb564e972ec732e4431
Kevin <kevinbczhang@gmail.com>
2022-08-18 22:54:32 -0400

    Particle Sprites

---
+++ constants.py
@@ -0,0 +1,46 @@
+import os, pygame
+
+# sets up pygame
+pygame.init()
+pygame.font.init()
+pygame.display.set_caption('Novorus')
+
+# sets the size of the screen; defaults to full screen
+RESOLUTION = (1920, 1080)
+screen = pygame.display.set_mode(RESOLUTION)  # , pygame.FULLSCREEN)
+clock = pygame.time.Clock()
+
+# colors
+RED = (211, 47, 47)
+BLOOD_RED = (198, 40, 40)
+
+ORANGE = (255, 174, 66)
+TANGERINE = (212, 103, 0)
+
+MELLOW_YELLOW = (255, 229, 134)
+YELLOW = (255, 231, 45)
+GOLD = (255, 219, 14)
+
+WHITE = (255, 255, 255)
+LIGHT_GREY = (210, 210, 210)
+GREY = (188, 188, 188)
+DARK_GREY = (168, 168, 168)
+BLACK = (50, 50, 50)
+
+BROWN = (131, 106, 83)
+PECAN = (115, 93, 71)
+DARK_BROWN = (104, 84, 66)
+
+TILE_SIZE = 100
+STARTING_FLOOR = 1
+game_state = {'paused': False,
+              'runtime': True,
+              'fullscreen': True}
+
+IMAGES = {}
+for (path, dirs, files) in os.walk('sprites', topdown=True):
+    for file in files:
+        IMAGES[file] = path
+
+for file, path in IMAGES.items():
+    IMAGES[file] = pygame.image.load(os.path.join(path, file)).convert_alpha()
