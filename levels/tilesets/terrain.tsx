003451fcd115f7e8017a01c4cd6964882a0725c6
Kevin <kevinbczhang@gmail.com>
2022-10-09 00:34:32 -0400

    Too Much Time on Terrain Sprites

---
+++ levels/tilesets/terrain.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="grassy_texture" tilewidth="24" tileheight="24" tilecount="10" columns="10">
+ <image source="../../sprites/terrain/grassy_texture.png" width="240" height="24"/>
+</tileset>
