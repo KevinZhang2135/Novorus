4ab81861da68c1ff1c4f3ac01e42fae3f74da971
Kevin <kevinbczhang@gmail.com>
2023-06-25 23:08:31 -0400

    Removed Tilesets

--- levels/tileset/bricks.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="bricks" tilewidth="32" tileheight="32" tilecount="5" columns="1">
- <image source="bricks.png" width="32" height="160"/>
-</tileset>
--- levels/tileset/enemies.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="enemies" tilewidth="64" tileheight="64" tilecount="4" columns="1">
- <image source="enemies.png" width="64" height="256"/>
-</tileset>
